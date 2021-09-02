ee15cd727cc6c1dc65e40c4f7c2a09d83eacecb4
Blankeos <carloantonioct@gmail.com>
2021-09-02 18:26:46 +0800

    Setup Vite + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div className="">Hello World</div>;
+}
+
+export default App;
