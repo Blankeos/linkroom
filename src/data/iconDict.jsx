4b91bd6e2ef4a47a6d4670d62787859b920c61ca
Blankeos <carloantonioct@gmail.com>
2021-09-03 00:25:11 +0800

    Created an icon dict for parsing supported string icon names

---
+++ src/data/iconDict.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+  SiGoogleclassroom,
+  SiFacebook,
+  SiGooglehangoutsmeet,
+} from "react-icons/si";
+
+const iconDict = {
+  facebook: <SiFacebook />,
+  google_classroom: <SiGoogleclassroom />,
+  google_meet: <SiGooglehangoutsmeet />,
+};
+
+export default iconDict;
