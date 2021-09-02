518ae5829c810edfd8d48b05fc65eb0ac8100314
Blankeos <carloantonioct@gmail.com>
2021-09-02 21:26:04 +0800

    Cards are now renderable with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,104 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
-import "./App.css";
 import Card from "./components/Card";
+import PrimaryButton from "./components/PrimaryButton";
+import SecondaryButton from "./components/SecondaryButton";
+
+import { MdEdit, MdImportExport } from "react-icons/md";
+import {
+  SiGoogleclassroom,
+  SiFacebook,
+  SiGooglehangoutsmeet,
+} from "react-icons/si";
 
 function App() {
   return (
     <div className="">
+      <header className="bg-blue-500 h-20 flex items-center justify-center">
+        <div className="flex flex-col items-center">
+          <p className="text-white font-black tracking-wider text-xl">
+            Link Room
+          </p>
+          <p className="text-xs font-light text-blue-100">
+            One place for your all your virtual classrooms
+          </p>
+        </div>
+      </header>
       {/* Grid */}
-      <div className="grid grid-cols-2 gap-5 p-5">
-        <Card />
-        <Card />
-        <Card />
+      {/* <div className="grid 2xl:grid-cols-5 grid-cols-2 gap-5 p-5"> */}
+      <div
+        className="grid justify-center gap-5 p-5"
+        style={{
+          gridTemplateColumns: "repeat(auto-fit, minmax(24rem, max-content))",
+        }}
+      >
+        <Card
+          title={"CCS 248"}
+          subheading1={"Artificial Neural Networks"}
+          subheading2={"Bobby Gerardo"}
+          links={[
+            {
+              linkName: "Google Classroom",
+              icon: <SiGoogleclassroom />,
+              url: "/gcr",
+            },
+            { linkName: "Facebook", icon: <SiFacebook />, url: "/Fb" },
+            {
+              linkName: "Google Meet",
+              icon: <SiGooglehangoutsmeet />,
+              url: "/gm",
+            },
+          ]}
+        />
+        <Card
+          title={"CCS 248"}
+          subheading1={"Artificial Neural Networks"}
+          subheading2={"Bobby Gerardo"}
+          links={[
+            {
+              linkName: "Google Classroom",
+              icon: <SiGoogleclassroom />,
+              url: "/gcr",
+            },
+            { linkName: "Facebook", icon: <SiFacebook />, url: "/Fb" },
+            {
+              linkName: "Google Meet",
+              icon: <SiGooglehangoutsmeet />,
+              url: "/gm",
+            },
+          ]}
+        />
+        <Card
+          title={"CCS 248"}
+          subheading1={"Artificial Neural Networks"}
+          subheading2={"Bobby Gerardo"}
+          links={[
+            {
+              linkName: "Google Classroom",
+              icon: <SiGoogleclassroom />,
+              url: "/gcr",
+            },
+            { linkName: "Facebook", icon: <SiFacebook />, url: "/Fb" },
+            {
+              linkName: "Google Meet",
+              icon: <SiGooglehangoutsmeet />,
+              url: "/gm",
+            },
+          ]}
+        />
+      </div>
+      {/* </div> */}
+
+      <div className="p-5 flex space-x-3">
+        <SecondaryButton className="flex items-center space-x-1">
+          <MdImportExport size="1.2rem" />
+          <span className="pr-1">Import/Export</span>
+        </SecondaryButton>
+
+        <PrimaryButton className="flex items-center space-x-1">
+          <MdEdit />
+          <span className="pr-1">Edit</span>
+        </PrimaryButton>
       </div>
     </div>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,18 +5,22 @@ import {
   SiGooglehangoutsmeet,
 } from "react-icons/si";
 
-const Card = () => {
+const Card = ({ title, subheading1, subheading2, links }) => {
   return (
-    <div className="rounded-md shadow">
+    <div className="rounded-md shadow hover:shadow-lg transition w-96 inline-block">
       <div className="flex flex-col space-y-1 bg-blue-500 text-white p-5 rounded-t-md">
-        <h2 className="font-bold text-3xl">Card Title</h2>
-        <p className="text-sm">CCS248</p>
-        <p className="text-sm">Bobby Gerardo</p>
+        <h2 className="font-bold text-3xl">{title}</h2>
+        <p className="text-sm">{subheading1}</p>
+        <p className="text-sm">{subheading2}</p>
       </div>
       <div className="p-5 flex flex-col space-y-2 text-gray-600 border-b border-r border-l rounded-b-md">
-        <Link icon={<SiGoogleclassroom />}>Google Class</Link>
-        <Link icon={<SiFacebook />}>Facebook Group</Link>
-        <Link icon={<SiGooglehangoutsmeet />}>Google Meet</Link>
+        {links.map((link, i) => {
+          return (
+            <Link key={i} icon={link && link.icon}>
+              {link.linkName}
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
@@ -24,7 +28,10 @@ const Card = () => {
 
 const Link = ({ url, icon, children }) => {
   return (
-    <a href={url || "/404"} className="flex items-center space-x-2">
+    <a
+      href={url || "/404"}
+      className="flex items-center space-x-2 hover:text-blue-500"
+    >
       {icon}
       <span>{children}</span>
     </a>
