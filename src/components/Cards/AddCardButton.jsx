ccbd045febaab61609e9e169399dd501c3d9f7ec
Blankeos <carloantonioct@gmail.com>
2021-09-05 03:02:07 +0800

    Created Card that serves as add button in edit mode

---
+++ src/components/Cards/AddCardButton.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { MdAdd as AddIcon } from "react-icons/md";
+import { useCardsContext } from "../../contexts/CardsContext";
+
+const AddCardButton = () => {
+  const { addNewCard } = useCardsContext();
+  return (
+    <button
+      onClick={addNewCard}
+      className="group rounded-2xl hover:shadow-lg transition w-full sm:w-96 inline-block border border-blue-500 hover:bg-blue-500 text-blue-500"
+      style={{ minHeight: "18rem" }}
+    >
+      <div className="w-full h-full flex justify-center items-center">
+        <span className="group-hover:border-white transition p-3 border border-blue-500 rounded-full">
+          <AddIcon
+            className="group-hover:text-white transition"
+            size="1.5rem"
+          />
+        </span>
+      </div>
+    </button>
+  );
+};
+
+export default AddCardButton;
