b7a7fca34493f0121d5b7de74f50c29b5451ef6a
Blankeos <carloantonioct@gmail.com>
2021-09-04 17:58:25 +0800

    Default link icon added

--- src/data/iconDict.jsx
+++ src/data/iconDict.jsx
@@ -5,10 +5,12 @@ import {
   SiGooglehangoutsmeet,
 } from "react-icons/si";
 
+import { BiLink } from "react-icons/bi";
 const iconDict = {
   facebook: <SiFacebook />,
   google_classroom: <SiGoogleclassroom />,
   google_meet: <SiGooglehangoutsmeet />,
+  default_link: <BiLink />,
 };
 
 export default iconDict;
