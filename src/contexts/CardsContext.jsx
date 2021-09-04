a3da44392bb9eb2e4e693be8523c51f996865d65
Blankeos <carloantonioct@gmail.com>
2021-09-04 19:29:02 +0800

    Reducer state management for whole cards state

--- src/contexts/CardsContext.jsx
+++ src/contexts/CardsContext.jsx
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useContext, createContext } from "react";
+import React, {
+  useState,
+  useEffect,
+  useContext,
+  createContext,
+  useReducer,
+} from "react";
 
 export const CARDS_STORAGE = "cardsStorage";
 
@@ -8,32 +14,95 @@ const CardsContext = createContext(initialValue);
 
 export const useCardsContext = () => useContext(CardsContext);
 
+const reducer = (state, action) => {
+  let newCards;
+
+  switch (action.type) {
+    case "SET_ALL":
+      return action.payload;
+    case "SET_ONE_CARD_ALL":
+      newCards = [...state.cards];
+      newCards[action.payload.id] = action.payload.data;
+
+      return {
+        cards: newCards,
+      };
+    case "SET_ONE_CARD_PROPERTY":
+      newCards = [...state.cards];
+      newCards[action.payload.id] = {
+        ...state.cards[action.payload.id],
+        [action.payload.propertyName]: action.payload.data,
+      };
+
+      return {
+        cards: newCards,
+      };
+    default:
+      return state;
+  }
+};
+
 export const CardsProvider = ({ children }) => {
-  const [cards, setCards] = useState(null);
+  const [cards, dispatch] = useReducer(reducer, null);
+  const [isEditingAllCards, setIsEditingAllCards] = useState(false);
 
   useEffect(() => {
     if (localStorage.getItem(CARDS_STORAGE) !== null) {
       //   Found existing cards_storage
       const cardsData = JSON.parse(localStorage.getItem(CARDS_STORAGE));
-      setCards(cardsData);
+      dispatch({ type: "SET_ALL", payload: cardsData });
     } else {
       // Make a new cards_storage
       const cardsData = {};
       saveToStorage(cardsData);
-      setCards(cardsData);
+      dispatch({ type: "SET_ALL", payload: cardsData });
     }
   }, []);
 
+  useEffect(() => {
+    console.log(cards);
+  }, [cards]);
+
+  // Public Functions
   const saveToStorage = (cardsObj) => {
     localStorage.setItem(CARDS_STORAGE, JSON.stringify(cardsObj));
   };
 
   const importCards = (cardsObj) => {
-    setCards(cardsObj);
+    dispatch({ type: "SET_ALL", payload: cardsObj });
     saveToStorage(cardsObj);
   };
 
-  const value = { cards, importCards };
+  const toggleIsEditingAllCards = () => {
+    setIsEditingAllCards((prev) => {
+      // If we were currently editing, that means we're going to save since we're done.
+      if (prev) {
+        console.log("Trying to save");
+        saveToStorage(cards);
+      }
+
+      return !prev;
+    });
+  };
+
+  const changeOneCardTitle = (id, propertyName, data) => {
+    dispatch({
+      type: "SET_ONE_CARD_PROPERTY",
+      payload: {
+        id: id,
+        propertyName: propertyName,
+        data: data,
+      },
+    });
+  };
+
+  const value = {
+    cards,
+    importCards,
+    isEditingAllCards,
+    toggleIsEditingAllCards,
+    changeOneCardTitle,
+  };
   return (
     <CardsContext.Provider value={value}>{children}</CardsContext.Provider>
   );
