e8302ba60740f0eb8f81c716607fe712e5f03d13
Blankeos <carloantonioct@gmail.com>
2021-09-05 03:01:16 +0800

    CardsGrid is responsive and now uses Context values for rendering

--- src/components/Cards/CardsGrid.jsx
+++ src/components/Cards/CardsGrid.jsx
@@ -3,45 +3,50 @@ import Card from "./Card";
 import { useCardsContext } from "../../contexts/CardsContext";
 
 import { GiBroom } from "react-icons/gi";
+import { MdAdd as AddIcon } from "react-icons/md";
+
 import EditableCard from "./EditableCard";
+import AddCardButton from "./AddCardButton";
 
 const CardsGrid = () => {
-  const { cards, isEditingAllCards } = useCardsContext();
-  // const [editing, setEditing] = useState(false);
+  const { cards, isEditingAllCards, addNewCard, toggleIsEditingAllCards } =
+    useCardsContext();
 
-  const renderGrid = (cards) => {
-    if (cards) {
+  const renderEditableCards = (cards) => {
+    const editableCards = cards.map((card, i) => {
       return (
-        <div
-          className="grid justify-center gap-5 p-5"
-          style={{
-            gridTemplateColumns: "repeat(auto-fit, minmax(24rem, max-content))",
-          }}
-        >
-          {cards.map((card, i) => {
-            if (isEditingAllCards) {
-              return (
-                <EditableCard
-                  key={i}
-                  index={i}
-                  title={card.title}
-                  subheading1={card.subheading1}
-                  subheading2={card.subheading2}
-                  links={card.links}
-                />
-              );
-            } else {
-              return (
-                <Card
-                  key={i}
-                  title={card.title}
-                  subheading1={card.subheading1}
-                  subheading2={card.subheading2}
-                  links={card.links}
-                />
-              );
-            }
-          })}
+        <EditableCard
+          key={i}
+          index={i}
+          title={card.title}
+          subheading1={card.subheading1}
+          subheading2={card.subheading2}
+          links={card.links}
+        />
+      );
+    });
+
+    return [editableCards, <AddCardButton key={-1} />];
+  };
+
+  const renderCards = (cards) => {
+    return cards.map((card, i) => {
+      return (
+        <Card
+          key={i}
+          title={card.title}
+          subheading1={card.subheading1}
+          subheading2={card.subheading2}
+          links={card.links}
+        />
+      );
+    });
+  };
+  const renderGrid = (cards) => {
+    if (cards != null && cards.length > 0) {
+      return (
+        <div className="grid sm:justify-center gap-5 p-5 grid-cols-1 cards-grid">
+          {isEditingAllCards ? renderEditableCards(cards) : renderCards(cards)}
         </div>
       );
     } else {
@@ -50,27 +55,24 @@ const CardsGrid = () => {
           <GiBroom className="text-3xl text-gray-400" />
           <span>No cards yet.</span>
           <span>Create a new one to get started!</span>
+          <span>
+            <button
+              className="mt-10 p-10 rounded-2xl hover:shadow-lg flex items-center justify-center space-x-2 border border-blue-500 hover:bg-blue-500 text-blue-500 hover:text-white"
+              onClick={() => {
+                addNewCard();
+                toggleIsEditingAllCards();
+              }}
+            >
+              <AddIcon size="1.2rem" />
+              <span>Create My First Card</span>
+            </button>
+          </span>
         </div>
       );
     }
   };
 
-  return (
-    <>
-      {cards && (
-        <>
-          {renderGrid(cards.cards)}{" "}
-          {/* <button
-            onClick={() => {
-              setEditing((prev) => !prev);
-            }}
-          >
-            SetEditing
-          </button> */}
-        </>
-      )}
-    </>
-  );
+  return <>{cards && <>{renderGrid(cards.cards)}</>}</>;
 };
 
 export default CardsGrid;
