a15db9b59a7fff43c10bcc4b0f49293f6dcea97e
Blankeos <carloantonioct@gmail.com>
2021-09-14 01:14:07 +0800

    Empty Grid has illustrations

--- src/components/EmptyGrid.jsx
+++ src/components/EmptyGrid.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { GiBroom } from "react-icons/gi";
 import { MdAdd as AddIcon } from "react-icons/md";
 import { useCardsContext } from "../contexts/CardsContext";
 
@@ -7,9 +6,20 @@ const EmptyGrid = () => {
   const { addNewCard, toggleIsEditingAllCards } = useCardsContext();
   return (
     <div className="flex-grow text-center flex flex-col space-y-1 justify-center items-center text-gray-400 text-sm font-light pb-16">
-      <GiBroom className="text-3xl text-gray-400" />
-      <span>No cards yet.</span>
-      <span>Create a new one to get started!</span>
+      {/* <img src="/assets/socket_illustration_0.svg" /> */}
+      <div
+        className="w-64 h-64"
+        style={{
+          backgroundImage: "url('/assets/socket_illustration_0.svg')",
+          backgroundSize: "contain",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      ></div>
+      <span>
+        <b>Socket</b> is cleaning up your empty room.
+      </span>
+      <span>Create a new card to get started!</span>
       <span>
         <button
           className="mt-10 p-10 rounded-2xl hover:shadow-lg flex items-center justify-center space-x-2 border border-blue-500 hover:bg-blue-500 text-blue-500 hover:text-white transition"
