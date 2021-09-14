1e7128e001fe89c767a6d91e0db4cc07c95e2e07
Blankeos <carloantonioct@gmail.com>
2021-09-15 01:46:45 +0800

    Enabled jit tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.jsx"],
   darkMode: false, // or 'media' or 'class'
+  mode: "jit",
   theme: {
     extend: {
       cursor: {
