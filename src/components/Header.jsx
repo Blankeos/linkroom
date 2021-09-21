c00110f1f3e67d6a8921c11599a2f119cb09e864
Blankeos <carloantonioct@gmail.com>
2021-09-21 21:01:34 +0800

    Added sidenav button on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import SideNavButton from "./SideNavButton";
+
+// Icons
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
 
 const Header = () => {
   return (
-    <header className="bg-blue-500 h-20 flex items-center justify-center">
-      <div className="flex flex-col items-center">
+    <header className="relative bg-blue-500 h-20 flex items-center">
+      <div className="absolute flex flex-col items-center w-full">
         <p className="text-white font-black tracking-wider text-xl relative">
           Link Room
           <span
@@ -16,6 +20,13 @@ const Header = () => {
         <p className="text-xs font-light text-blue-100">
           One place for your all your virtual classrooms
         </p>
+      </div>
+      <div className="absolute right-0 text-white p-5">
+        <SideNavButton>
+          <span className="block p-1">
+            <HiOutlineMenuAlt3 size="1.7rem" />
+          </span>
+        </SideNavButton>
       </div>
     </header>
   );
