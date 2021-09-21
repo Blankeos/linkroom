e119d2d57774f928164d57ec88c0a95adfec4fc9
Blankeos <carloantonioct@gmail.com>
2021-09-21 20:59:37 +0800

    Changed AddCardButton's callback to open edit modal instead

--- src/components/Cards/AddCardButton.jsx
+++ src/components/Cards/AddCardButton.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { MdAdd as AddIcon } from "react-icons/md";
 import { useCardsContext } from "../../contexts/CardsContext";
+import { useEditModalContext } from "../../contexts/EditModalContext";
 
 const AddCardButton = () => {
-  const { addNewCard } = useCardsContext();
+  // const { addNewCard } = useCardsContext(); // only useful when isEditingAllCards
+  const { showEditModal } = useEditModalContext();
+
   return (
     <button
-      onClick={addNewCard}
+      onClick={() => showEditModal()}
       className="group rounded-2xl hover:shadow-lg transition w-full sm:w-96 inline-block border border-blue-500 hover:bg-blue-500 text-blue-500"
       style={{ minHeight: "18rem" }}
     >
