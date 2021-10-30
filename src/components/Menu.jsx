e3bad6ba7eff1c7dbd66b93fee4bdc1cc6495b08
Blankeos <carloantonioct@gmail.com>
2021-10-30 17:36:29 +0800

    Added temporary switch toggle for dark mode

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,23 +5,17 @@ import { useCardsContext } from "../contexts/CardsContext";
 import PrimaryButton from "./PrimaryButton";
 import SecondaryButton from "./SecondaryButton";
 import ImportExportModal from "./Modals/ImportExportModal";
+import { Switch } from "@headlessui/react";
 
 // Icons
 import { MdImportExport } from "react-icons/md";
 import { MdEdit } from "react-icons/md";
 import { GoCheck } from "react-icons/go";
+import { useDarkThemeContext } from "../contexts/DarkThemeContext";
 
 const Menu = () => {
   const [bool, setBool] = useState(false);
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  useEffect(() => {
-    if (isDarkMode) {
-      document.body.classList.add("dark");
-    } else {
-      document.body.classList.remove("dark");
-    }
-  }, [isDarkMode]);
+  const { isDark, setIsDark } = useDarkThemeContext();
 
   const { toggleIsEditingAllCards, isEditingAllCards, cards } =
     useCardsContext();
@@ -35,14 +29,26 @@ const Menu = () => {
           <MdImportExport size="1.2rem" />
           <span className="pr-1">Import/Export</span>
         </SecondaryButton>
-        <SecondaryButton onClick={() => setIsDarkMode((prev) => !prev)}>
+        {/* <SecondaryButton onClick={() => setIsDarkMode((prev) => !prev)}>
           {isDarkMode ? "Disable Dark" : "Enable Dark"}
-        </SecondaryButton>
-
+        </SecondaryButton> */}
+        <Switch
+          checked={isDark}
+          onChange={setIsDark}
+          className={`${isDark ? "bg-[teal]" : "bg-[cyan]"}
+          relative inline-flex flex-shrink-0 h-[38px] w-[74px] border-2 border-gray-900 rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+        >
+          <span className="sr-only">Use setting</span>
+          <span
+            aria-hidden="true"
+            className={`${isDark ? "translate-x-9" : "translate-x-0"}
+            pointer-events-none inline-block h-[34px] w-[34px] rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200`}
+          />
+        </Switch>
         {/* {cards && cards.cards.length > 0 && (
           <PrimaryButton
-            onClick={() => toggleIsEditingAllCards()}
-            className="flex items-center space-x-1"
+          onClick={() => toggleIsEditingAllCards()}
+          className="flex items-center space-x-1"
           >
             {isEditingAllCards ? <GoCheck /> : <MdEdit />}
             <span className="pr-1">
