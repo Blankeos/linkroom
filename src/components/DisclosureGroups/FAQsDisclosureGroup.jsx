bc06dffeaa23381575f2a58ff824bc6b917e079e
Blankeos <carloantonioct@gmail.com>
2021-11-01 22:23:59 +0800

    Working Disclosure group (no content yet)

--- src/components/DisclosureGroups/FAQsDisclosureGroup.jsx
+++ src/components/DisclosureGroups/FAQsDisclosureGroup.jsx
@@ -1,45 +1,56 @@
 import React from "react";
 import { Disclosure, Transition } from "@headlessui/react";
 
+// Icons
+import {
+  BiChevronDown as ChevronDown,
+  BiChevronUp as ChevronUp,
+} from "react-icons/bi";
+
 export default function FAQsDisclosureGroup() {
   return (
-    <div className="w-full px-4 pt-16">
-      <div className="w-full max-w-md p-2 mx-auto bg-white rounded-2xl">
-        <Disclosure>
-          {({ open }) => (
-            <>
-              <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-purple-900 bg-purple-100 rounded-lg hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                <span>What is your refund policy?</span>
-              </Disclosure.Button>
-              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                If you're unhappy with your purchase for any reason, email us
-                within 90 days and we'll refund you in full, no questions asked.
-              </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
-        <Disclosure as="div" className="mt-2">
-          {({ open }) => (
-            <>
-              <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-purple-900 bg-purple-100 rounded-lg hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                <span>Do you offer technical support?</span>
-              </Disclosure.Button>
-              <Transition
-                enter="transition duration-100 ease-out"
-                enterFrom="transform scale-95 opacity-0"
-                enterTo="transform scale-100 opacity-100"
-                leave="transition duration-75 ease-out"
-                leaveFrom="transform scale-100 opacity-100"
-                leaveTo="transform scale-95 opacity-0"
-              >
-                <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                  No.
-                </Disclosure.Panel>
-              </Transition>
-            </>
-          )}
-        </Disclosure>
+    <div className="w-full px-4">
+      <div className="flex flex-col space-y-2 w-full max-w-md p-2 mx-auto bg-white rounded-2xl">
+        <MyDisclosure
+          panelContent="You can't fam sorry. At least, not for now. For now you only have 1 room to work with but you can put as many cards and links on it as you like."
+          buttonContent="Can I have more than 1 room for my links?"
+        />
+        <MyDisclosure
+          panelContent="For now, the app still works with local storage which means other people can't see your links."
+          buttonContent="How do I share my LinkRoom?"
+        />
+        <MyDisclosure
+          panelContent="Since the app uses local storage as the main source of data, we can't do any syncing functionalities yet. You can however 'export' your current LinkRoom and 'import' it on another device."
+          buttonContent="How do I sync my LinkRoom on another device?"
+        />
       </div>
     </div>
+  );
+}
+
+function MyDisclosure({ panelContent = "", buttonContent = "" }) {
+  return (
+    <Disclosure as="div" className="">
+      {({ open }) => (
+        <>
+          <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-purple-900 bg-purple-100 rounded-lg hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+            <span>{buttonContent}</span>
+            {open ? <ChevronUp size="1.1rem" /> : <ChevronDown size="1.1rem" />}
+          </Disclosure.Button>
+          <Transition
+            enter="transition duration-100 ease-out"
+            enterFrom="transform scale-95 opacity-0"
+            enterTo="transform scale-100 opacity-100"
+            leave="transition duration-75 ease-out"
+            leaveFrom="transform scale-100 opacity-100"
+            leaveTo="transform scale-95 opacity-0"
+          >
+            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+              {panelContent}
+            </Disclosure.Panel>
+          </Transition>
+        </>
+      )}
+    </Disclosure>
   );
 }
