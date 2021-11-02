395b08eb90801d38a0d6556a9c147b17d3223742
Blankeos <carloantonioct@gmail.com>
2021-11-03 00:30:01 +0800

    Added working mobile nav + dark mode

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { isMobile } from "react-device-detect";
 
+// Components
+import { Link } from "react-router-dom";
+
 // Icons
 import { RiInformationLine as InfoIcon } from "react-icons/ri";
 import { BsHouse as HomeIcon, BsArrowRepeat as SortIcon } from "react-icons/bs";
@@ -16,22 +19,31 @@ const DesktopFooter = () => {
 
 const MobileFooter = () => {
   return (
-    <div className="fixed bottom-0">
-      <div className="h-16 bg-gray-50 border-t w-full grid grid-cols-3 fixed bottom-0 text-gray-600">
-        <button className="flex flex-col items-center justify-center h-full">
-          <HomeIcon size="1.3rem" />
-          <span className="text-xs">Home</span>
-        </button>
-        <button className="flex flex-col items-center justify-center h-full">
-          <SortIcon size="1.3rem" />
-          <span className="text-xs">Sort</span>
-        </button>
-        <button className="flex flex-col items-center justify-center h-full">
-          <InfoIcon size="1.3rem" />
-          <span className="text-xs">Info</span>
-        </button>
+    <>
+      <div className="h-16 bg-gray-50 border-t w-full grid grid-cols-3 text-gray-600 dark:bg-gray-900 dark:border-black dark:text-gray-400 transition" />
+      <div className="fixed bottom-0 w-full">
+        <div className="h-16 bg-gray-50 border-t w-full grid grid-cols-3 text-gray-600 dark:bg-gray-900 dark:border-black dark:text-gray-400 transition">
+          <Link
+            to="/"
+            className="flex flex-col items-center justify-center h-full"
+          >
+            <HomeIcon size="1.3rem" />
+            <span className="text-xs">Home</span>
+          </Link>
+          <button className="flex flex-col items-center justify-center h-full">
+            <SortIcon size="1.3rem" />
+            <span className="text-xs">Sort</span>
+          </button>
+          <Link
+            to="/about"
+            className="flex flex-col items-center justify-center h-full"
+          >
+            <InfoIcon size="1.3rem" />
+            <span className="text-xs">Info</span>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 export default Footer;
