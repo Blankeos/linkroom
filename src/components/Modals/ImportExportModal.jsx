b1d1211f91dafc3c723ea38bf01c68dad4acbc81
Blankeos <carloantonioct@gmail.com>
2021-11-15 17:16:38 +0800

    ImportExportModal implements validation

--- src/components/Modals/ImportExportModal.jsx
+++ src/components/Modals/ImportExportModal.jsx
@@ -10,50 +10,102 @@ import Card from "../Cards/Card";
 import { RiInformationLine as InfoIcon } from "react-icons/ri";
 import { IoMdClose as CloseIcon } from "react-icons/io";
 import { HiUpload as ImportIcon } from "react-icons/hi";
-import { BiCopy as CopyIcon } from "react-icons/bi";
+import {
+  BiCopy as CopyIcon,
+  BiError as ErrorIcon,
+  BiCheckCircle as CheckIcon,
+} from "react-icons/bi";
 import { BsCheck as CopiedIcon } from "react-icons/bs";
 
 import Tippy from "@tippyjs/react";
 import "tippy.js/dist/tippy.css";
 
+// UtilityFunctions
+import cardsValidate, { isValid } from "../../data/cardsValidate";
+import { toast } from "react-toastify";
+
 const ImportExportModal = ({ isOpen, closeModal }) => {
   const { cards, importCards } = useCardsContext();
   const [importInput, setImportInput] = useState("");
 
+  const [errors, setErrors] = useState([]); // List of strings to be displayed under import text area
+  const [importIsValid, setImportIsValid] = useState(false); // bool for displaying green ring around text area when valid
+
   // Copy Button State
   const [copyButtonText, setCopyButtonText] = useState("Copy");
 
-  function CardsJSONFormatError() {}
-  CardsJSONFormatError.prototype = new Error();
+  const handleClose = () => {
+    // Reset states after 0.5s. (Wait for modal close animation)
+    setTimeout(() => {
+      setImportInput("");
+      setErrors([]);
+      setImportIsValid(false);
+    }, 500);
 
-  const handleSave = () => {
+    closeModal();
+  };
+
+  const validateImport = (onValid = (data) => {}, onInvalid = () => {}) => {
     let data;
     try {
       // Parsing JSON string
       data = JSON.parse(importInput);
 
-      // Checking if valid format?
-      data.cards.map((card, i) => {
-        return (
-          <Card
-            key={card._id}
-            title={card.title}
-            subheading1={card.subheading1}
-            subheading2={card.subheading2}
-            links={card.links}
-          />
+      if (isValid(data)) {
+        onValid(data);
+        setErrors([]);
+        setImportIsValid(true);
+      } else {
+        setErrors(
+          cardsValidate.errors.map(
+            (err) => `${err.instancePath} ${err.message}`
+          )
         );
-      });
-
-      // Saving to json file
-      importCards(data);
-    } catch (e) {
-      if (e instanceof SyntaxError) {
+        setImportIsValid(false);
+        onInvalid();
+      }
+    } catch (err) {
+      if (err instanceof SyntaxError) {
         console.log("Failed to parse import (Might not be a JSON file)");
-      } else if (e instanceof TypeError) {
+        setErrors(["Incorrect JSON format"]);
+      } else if (err instanceof TypeError) {
+        // This error might not be raised because we're using isValid() to check properties
         console.log("Can't read any properties of imported JSON");
       }
+      setImportIsValid(false);
+      onInvalid();
     }
+  };
+
+  const handleSave = () => {
+    validateImport(
+      (data) => {
+        importCards(data);
+        toast.success("Successfully Imported 🎉", {
+          position: "bottom-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
+        handleClose();
+      },
+      () => {
+        toast.error("Failed To Import 😭", {
+          position: "bottom-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
+      }
+    );
   };
 
   const handleCopy = () => {
@@ -68,17 +120,17 @@ const ImportExportModal = ({ isOpen, closeModal }) => {
     <Modal
       modalClass="w-full sm:w-11/12 md:w-9/12 lg:w-7/12 p-10 dark:bg-gray-900"
       isOpen={isOpen}
-      closeModal={closeModal}
+      closeModal={handleClose}
     >
       <div className="flex justify-end">
-        <button onClick={closeModal}>
+        <button onClick={handleClose}>
           <CloseIcon
             className="text-gray-400 hover:text-gray-500"
             size="1.2rem"
           />
         </button>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-[2fr,1fr] gap-6">
         <div>
           <span className="flex space-x-2 items-center mb-2 text-lg text-gray-700 dark:text-gray-200 font-semibold">
             <span>Import</span>
@@ -91,9 +143,35 @@ const ImportExportModal = ({ isOpen, closeModal }) => {
           <div className="h-52  text-gray-600">
             <textarea
               onChange={(e) => setImportInput(e.target.value)}
-              className="p-2 h-full w-full resize-none border border-blue-100 focus:ring-1 rounded focus:ring-blue-500 outline-none focus:outline-none dark:bg-gray-700 dark:text-gray-200 dark:border-gray-700"
+              className={`p-2 h-full w-full resize-none border border-blue-100 rounded outline-none focus:outline-none dark:bg-gray-700 dark:text-gray-200 dark:border-gray-700 ${
+                errors.length > 0
+                  ? "ring-1 ring-red-400"
+                  : importIsValid
+                  ? "ring-1 ring-green-400"
+                  : "focus:ring-1 focus:ring-blue-500"
+              }`}
               value={importInput}
+              onBlur={() => validateImport()}
             ></textarea>
+          </div>
+          <div className="mt-2.5 text-sm">
+            {errors.length > 0 &&
+              errors.map((err, i) => (
+                <span
+                  className="flex items-center space-x-1 text-red-400"
+                  key={i}
+                >
+                  <ErrorIcon size="1.15rem" />
+                  <span>{err}.</span>
+                </span>
+              ))}
+            {importIsValid && (
+              <span className="flex items-center space-x-1 text-green-400">
+                <CheckIcon size="1.15rem" />
+                <span>Good to go!</span>
+              </span>
+            )}
+            {/* <span className="text-green-400">Valid</span> */}
           </div>
           <ModalButton className="mt-3" onClick={handleSave}>
             <ImportIcon size="1.1rem" />
@@ -128,11 +206,12 @@ const ImportExportModal = ({ isOpen, closeModal }) => {
   );
 };
 
-const ModalButton = ({ children, onClick, className }) => {
+const ModalButton = ({ children, onClick, className, disabled = false }) => {
   return (
     <button
       type="button"
-      className={` flex items-center space-x-1 px-3 py-2 text-sm font-medium text-white bg-blue-500 border border-transparent rounded-md hover:bg-blue-400 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500  ${className}`}
+      disabled={disabled}
+      className={` flex items-center space-x-1 px-3 py-2 text-sm font-medium text-white bg-blue-500 border border-transparent rounded-md hover:bg-blue-400 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500  ${className} `}
       onClick={onClick}
     >
       {children}
